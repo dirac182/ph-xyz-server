a58205c25f3db2ff88cfec14414e3800761f43ac
Derrick <dmorin9696@yahoo.com>
2023-11-07 15:43:20 -0600

    again

--- users_api.js
+++ users_api.js
@@ -31,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use(cookieSession({
     sameSite: 'lax',
-    secure: false,
+    secure: true,
     httpOnly: true,
     maxAge: 30*24*60*60*1000,
     keys: [process.env.COOKIE_KEY]
